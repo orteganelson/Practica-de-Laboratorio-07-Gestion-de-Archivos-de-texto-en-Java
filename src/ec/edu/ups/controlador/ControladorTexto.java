bcaaeb158b225bd9d0a4e4ce7dabb0bb405cdd0a
AnahiCb <64875390+AnahiCb@users.noreply.github.com>
2020-07-12 08:26:02 -0500

    Update ControladorTexto.java
    Documentación del controlador

--- src/ec/edu/ups/controlador/ControladorTexto.java
+++ src/ec/edu/ups/controlador/ControladorTexto.java
@@ -12,10 +12,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 /**
+ * Esta clase contiene los métodos necesarias para el usuario de este
+ * programa pueda utilizarlo de una manera correcta y fácil, asi como los 
+ * metodos para controlar textosy que el usuario pueda manipular el programa con
+ * restricciones.
  *
- * @author Usuario
+ * @author Nelson Ortega
+ * @version 11.3
+ * @author Anahi Cabrera
+ * @version 8.2
  */
 public class ControladorTexto {
+     /**
+     * Declaracion de atributos
+     */
     private String ruta;
     private File fichero;
     
