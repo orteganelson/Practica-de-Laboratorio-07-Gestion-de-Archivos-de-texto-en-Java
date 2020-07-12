f82514b967605009ff68ac8c7ea75148b5fc8a96
AnahiCb <64875390+AnahiCb@users.noreply.github.com>
2020-07-12 10:18:23 -0500

    Update ControladorTexto.java
    Finalización de documentación de la clase ControladorTexto

--- src/ec/edu/ups/controlador/ControladorTexto.java
+++ src/ec/edu/ups/controlador/ControladorTexto.java
@@ -18,7 +18,7 @@ import java.util.*;
  * restricciones.
  *
  * @author Nelson Ortega
- * @version 11.3
+ * @version 8.2
  * @author Anahi Cabrera
  * @version 8.2
  */
@@ -30,6 +30,12 @@ public class ControladorTexto {
     private File fichero;
     private List<Character> abecedario;
     private Map<Character, Character> diccionario;
+     
+    /**
+     * Metodo constructor controladorTexto: hace la instancia de los atributos abecedario,
+     * diccionario crearDicionario. Este último servirá para cada vez que se cree un objeto 
+     * de esta clase.
+     */
     
     public ControladorTexto() {
         abecedario = new ArrayList<>();
@@ -37,6 +43,12 @@ public class ControladorTexto {
         diccionario = crearDiccionario();
     }
     
+     /**
+     * El metodo crearDiccionario no recibe ningun tipo de parámetro por ello
+     * declaramos nuevas variables de tipo String e int.
+     *
+     * @return diccionario
+     */
     public Map<Character, Character> crearDiccionario() {
         //Declaracion de nuevas varibles
         String abecedario = "abcdefghijklmnñopqrstuvwxyz";
@@ -44,7 +56,7 @@ public class ControladorTexto {
         String numeros = "0123456789";
         String numeros2="♫►◄↕¶§▬↨↑↓♪";        
         String espacio = " ";
-
+       
 
         for (int i = 0; i < abecedario.length(); i++) {
             diccionario.put(abecedario.charAt(i), abecedario2.charAt(i));
@@ -56,7 +68,16 @@ public class ControladorTexto {
         diccionario.put(espacio.charAt(0), numeros2.charAt(10));
         return diccionario;
     }
-    
+   /**
+     * El metodo encriptar tiene una variable auxiliar que sirve para
+     *  asignar a los sindicales, nuevos datos de tipo String. Este método, mediante la clase File y sus métodos de leer
+     * y escribir, nos permite realizar la encrptación del texto.
+     *
+     * @param rutaAbsoluta 
+     * @param texto 
+     * @throws Error: ruta no encontrada, escritura, general
+     */
+        
     public void encriptar(String rutaAbsoluta, String texto) {
         String aux = "";
         for (int i = 0; i < texto.length(); i++) {
@@ -95,6 +116,14 @@ public class ControladorTexto {
         }
     }
     
+     /**
+     * Este metodo recibe como parametro un dato de tipo String ruta, y
+     * comprueba que la ruta ingresada sea correcta y exista en el computador.
+     *
+     * @param ruta 
+     * @return Boolean (Si la ruta existe retornará true).
+     */
+    
     public boolean comprobarRuta(String ruta) {
         fichero = new File(ruta);
 
@@ -104,7 +133,15 @@ public class ControladorTexto {
             return false;
         }
     }
-    
+    /**
+     * El metodo concatena el nombre del archivo con "txt" y comprueba que el
+     * fichero exista en la ruta y el File.separator indica separador de directorios 
+     * que existe en el sistema operativo del computador que se esté utilizando.
+     *
+     * @param ruta
+     * @param nombre 
+     * @return Boolean (Retorna true or false dependiendo si la ruta existe)
+     */
     public boolean comprobar(String ruta, String nombre) {
         nombre = nombre.concat(".txt");
         
@@ -118,7 +155,14 @@ public class ControladorTexto {
             
         }
     }
-    
+     /**
+     * El metodo crearFichero, se crean ficheros utilizando el método createNewFile y además, mediante excepciones,
+     * controla el buen uso de la aplicación.
+     *
+     * @param ruta 
+     * @param nombre 
+     * @return Retorna la ruta absoluta de un arhivo o directorio
+     */
     public String crearFichero(String ruta, String nombre) {
 
         nombre = nombre.concat(".txt");
